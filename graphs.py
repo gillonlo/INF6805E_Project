e961ce09ad1d5c6397a23ff2e127cb7489cf55f2
Loic Grumiaux <loic.grumiaux@student.uclouvain.be>
2023-04-25 02:42:01 +0200

    First round. Fix #1.

--- graphs.py
+++ graphs.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 
+EXP_LENGTH=5000
+
 def metp2postfix(l):
     has = lambda x:("_"+str(l.get(x,"")) if l.get(x,False) else "")
     if l:
@@ -77,8 +79,8 @@ def m_plot_infection_evolution(filename, varying_parameter, list_of_mparam):
         subplot_type = "single_infected_"+metp2postfix(kwargs)
         infected = lazy_load(filename,subplot_type,**kwargs)
     
-        infected["nb_state"][:10000].plot(label=f"{varying_parameter}={kwargs[varying_parameter]}")
-        plt.fill_between(np.arange(0,10000,1),0,infected["nb_state"][:10000],alpha=.1)
+        infected["nb_state"][:EXP_LENGTH].plot(label=f"{varying_parameter}={kwargs[varying_parameter]}")
+        plt.fill_between(np.arange(0,EXP_LENGTH,1),0,infected["nb_state"][:EXP_LENGTH],alpha=.1)
 
     #plt.xscale("log")
     #plt.yscale("log")
@@ -96,7 +98,7 @@ def m_plot_removed_evolution(filename, varying_parameter, list_of_mparam):
         subplot_type = "single_removed_"+metp2postfix(kwargs)
         infected = lazy_load(filename,subplot_type,**kwargs)
     
-        infected["nb_state"][:10000].plot(label=f"{varying_parameter}={kwargs[varying_parameter]}")
+        infected["nb_state"][:EXP_LENGTH].plot(label=f"{varying_parameter}={kwargs[varying_parameter]}")
 
 
     #plt.xscale("log")
@@ -113,8 +115,8 @@ def m_mixed_infection_evolution(filenames, list_of_mparam):
     for i,kwargs in enumerate(list_of_mparam):
         subplot_type = "single_infected_"+metp2postfix(kwargs)
         infected = lazy_load(filenames[i],subplot_type,**kwargs)
-        infected["nb_state"][:10000].plot(label=f"{filenames[i]}")
-        plt.fill_between(np.arange(0,10000,1),0,infected["nb_state"][:10000],alpha=.1)  
+        infected["nb_state"][:EXP_LENGTH].plot(label=f"{filenames[i]}")
+        plt.fill_between(np.arange(0,EXP_LENGTH,1),0,infected["nb_state"][:EXP_LENGTH],alpha=.1)  
 
     plt.title(f"Removed evolution \nfor various models")
     plt.legend()
@@ -145,8 +147,14 @@ def gen_meta_meta(varying_parameter,values,exclusion=[],default={}):
                 tmp_m[k]=x
         l.append(tmp_m)
     return l
-    
 
+## EXP1
+"""
+m_plot_infection_evolution("EXP1","p_p_i_barrier",gen_meta_meta("p_p_i_barrier",[25,50,75,100],["t_infectious","t_removed"]))
+m_plot_infection_evolution("EXP1","n_clusters",gen_meta_meta("n_clusters",[2,3,4],["t_infectious","t_removed"]))
+m_plot_infection_evolution("EXP1","p_p_infected",gen_meta_meta("p_p_infected",[2,5,7,10,25],["t_infectious","t_removed"]))
+m_plot_infection_evolution("EXP1","p_i_infected",gen_meta_meta("p_i_infected",[10,20,30,50],["t_infectious","t_removed"]))
+"""
 
 ## EXP2
 #m_plot_infection_evolution("EXP2","t_infectious",gen_meta_meta("t_infectious",[100,250,500,1000],["t_removed"]))
@@ -162,10 +170,7 @@ def gen_meta_meta(varying_parameter,values,exclusion=[],default={}):
 #m_mixed_infection_evolution(["EXP2","EXP3"],[gen_meta_meta("t_infectious",[500],["t_removed"])[0],gen_meta_meta("t_infectious",[500],["t_removed"])[0]])
 
 # EXP5
-
-m_plot_infection_evolution("EXP5","t_infectious",gen_meta_meta("t_infectious",[100,250,500,1000],["t_removed"]))
-
-m_plot_infection_evolution("EXP5","p_p_infected",gen_meta_meta("p_p_infected",[2,5,7,10,25],["t_removed"]))
-
-m_plot_infection_evolution("EXP5","p_p_i_barrier",gen_meta_meta("p_p_i_barrier",[25,50,75,100],["t_removed"]))
+#m_plot_infection_evolution("EXP5","t_infectious",gen_meta_meta("t_infectious",[100,250,500,1000],["t_removed"]))
+#m_plot_infection_evolution("EXP5","p_p_infected",gen_meta_meta("p_p_infected",[2,5,7,10,25],["t_removed"]))
+m_plot_infection_evolution("EXP5","p_p_i_barrier",gen_meta_meta("p_p_i_barrier",[10,25,50,75],["t_removed"]))
 #m_plot_infection_evolution("EXP5","p_i_infected",gen_meta_meta("p_i_infected",[5,10,20,30,50],["t_removed"]))
